f38b84980ece4c2fb30b97a4b78cfedf07fd253e
Christopher Davis <cdavis9999@gmail.com>
2017-06-20 08:17:37 -0500

    Use npm To Install Dependencies

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-add_import_path "bower_components/bootstrap-sass/assets/stylesheets"
+add_import_path "node_modules/bootstrap-sass/assets/stylesheets"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -9,7 +9,7 @@ images_dir = "dist/images"
 javascripts_dir = "dist/scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
